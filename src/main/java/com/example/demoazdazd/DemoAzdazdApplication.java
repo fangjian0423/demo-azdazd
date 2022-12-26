78c2687d46cba62f212e6b90acc73cb0f71bf01b
fangjian0423 <SpringIntegSupport@microsoft.com>
2022-12-26 12:25:55 +0000

    Initial commit from Azure Spring Initializr

---
+++ src/main/java/com/example/demoazdazd/DemoAzdazdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoazdazd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAzdazdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAzdazdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoazdazd/DemoAzdazdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoazdazd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAzdazdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
